18de84ffa80dc1ae7a9e15e56ed920078909b5ea
Tuyen Tran <tuyen@Tuyens-MacBook-Pro.local>
2022-03-13 12:51:18 +0700

    Add workflows

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  cloud {
+    organization = "tuyentv"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "google" {
+  project = "TF_VAR_GCP_PROJECT_ID"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-a"
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
